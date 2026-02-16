7822f98968f13fac5a31b0eebf5814301cba04f3
pratham3252 <prathamsumesara09@gmail.com>
2026-02-16 16:18:44 +0530

    Add Firebase initialization script in Database.js

---
+++ Database.js
@@ -0,0 +1,16 @@
+<script>
+const firebaseConfig = {
+    apiKey: "AIzaSyCG7awgsnkNtyOoqN7aRsRyAP4hNxZsvdo",
+    authDomain: "chillbox-747f8.firebaseapp.com",
+    databaseURL: "https://chillbox-747f8-default-rtdb.firebaseio.com",
+    projectId: "chillbox-747f8",
+    storageBucket: "chillbox-747f8.firebasestorage.app",
+    messagingSenderId: "808846502692",
+    appId: "1:808846502692:web:fd3a70a823506207f261b1",
+    measurementId: "G-E4M4CH9BNY"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
